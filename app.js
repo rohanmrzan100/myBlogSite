2674fdc31169a806612e8dfa828161bb8cec9c7c
rohanmrzan100 <97383946+rohanmrzan100@users.noreply.github.com>
2022-07-22 22:27:30 +0545

    my changes

--- app.js
+++ app.js
@@ -126,8 +126,8 @@ app.post('/:post_id/edit',(req,res)=>{
     })
     res.redirect('/');
 })
-
-app.listen(process.env.PORT || 3000 ,()=>console.log("The server is running in port 3000"))
+const PORT = process.env.PORT || 3000
+app.listen(PORT ,()=>console.log("The server is running in port 3000"))
 
 
 
